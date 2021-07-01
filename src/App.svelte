ec3bc276ac47fa80306cf9c7e885d0b3ecaaafd7
trilusa <adriansalustri@gmail.com>
2021-07-01 14:28:41 -0400

    made all componets render dynamically from js obj

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,30 @@
 <script>
+	import { onMount } from "svelte";
+	import AudioPlayer from "./components/AudioPlayer.svelte";
 	import Meditation from "./components/Meditation.svelte";
 	import Nav from "./components/Nav.svelte";
-	import Reminders from "./components/Reminders.svelte";
+	import Table from "./components/Reminders.svelte";
 	import TextArea from "./components/TextArea.svelte";
+	
+	let components = [
+						{type: Nav, props: {}},
+						{type: Table, props: {table: [ {header: "Morning", entries: ["Meds", "Brush", "Water", "Sun"]},
+													   {header: "Afternoon", entries: ["Walk", "Meditate", "Water", "Lunch"]} ]}
+						},
+						{type: TextArea, props: {}},
+						{type: Meditation, props: {}}
+					];
 
 	window.onbeforeunload = function () {
   		return 'Refresh?';
  	}
 </script>
 
-<main>
-	<Nav/>
-	<Reminders/>
-	<TextArea/>
-	<Meditation/>
-</main>
+
+{#each components as component}
+	<svelte:component this={component.type} {...component.props}/>
+{/each}
+
 
 <style>
 	@media (min-width: 640px) {
--- src/components/Reminders.svelte
+++ src/components/Reminders.svelte
@@ -1,21 +1,24 @@
 <script>
-    let reminders = ["Meds", "Brush", "Water", "Sun"];
+    export let table; 
 </script>
 
-
-<div>
-    <h1>Good Morning!</h1>
-    <p>You got this man. Remember to be mindful.</p>
-    <h2>Reminders</h2>
-<!-- 
-    <span>
-    <label><input type=checkbox/> Meds</label>
-    <label><input type=checkbox/> Brush</label>
-    <label><input type=checkbox/> Water</label>
-    <label><input type=checkbox/> Light</label>
-    </span> -->
-
-    {#each reminders as reminder}
-        <label><input type=checkbox/> {reminder}</label>
+<div class="columns">
+    {#each table as column}
+        <div>
+        <h3>{column.header}</h3>
+            
+                {#each column.entries as entry}
+                    <label><input type=checkbox/> {entry}</label>
+                {/each}
+            
+        </div>
     {/each}
 </div>
+
+
+<style>
+    .columns{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    }
+</style>
